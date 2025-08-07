3e5434472b07408676ef24851657bc6e91104dc2
Frank A. Krueger <fak@praeclarum.org>
2025-08-06 19:53:16 -0700

    Implement getMotorConfigs

--- Config.h
+++ Config.h
@@ -41,8 +41,32 @@ public:
         {
     }
 
-    std::vector<MotorConfig*> getMotorConfigs() {
-        
+    inline std::vector<MotorConfig*> getMotorConfigs() {
+        std::vector<MotorConfig*> motors;
+        int num = numMotors.getInt();
+        if (num <= 0) {
+            ESP_LOGW("Config", "No motors configured");
+            return motors;
+        }
+        if (num > 8) {
+            ESP_LOGW("Config", "Number of motors exceeds maximum supported (8), clamping to 8");
+            num = 8;
+        }
+        motors.reserve(num);
+        for (int i = 0; i < num; ++i) {
+            switch (i) {
+                case 0: motors.push_back(&motor0); break;
+                case 1: motors.push_back(&motor1); break;
+                case 2: motors.push_back(&motor2); break;
+                case 3: motors.push_back(&motor3); break;
+                case 4: motors.push_back(&motor4); break;
+                case 5: motors.push_back(&motor5); break;
+                case 6: motors.push_back(&motor6); break;
+                case 7: motors.push_back(&motor7); break;
+                default: ESP_LOGW("Config", "Unexpected motor index %d, skipping", i);
+            }
+        }
+        return motors;
     }
 };
 
